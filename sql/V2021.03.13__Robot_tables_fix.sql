3cc671af9b70848619eb269c7ea65d842761a77f
Fabrizio Marconi <fabrizio.marconi@jef.it>
2021-03-12 15:03:47 +0100

    missing semicolomn

--- sql/V2021.03.13__Robot_tables_fix.sql
+++ sql/V2021.03.13__Robot_tables_fix.sql
@@ -1,5 +1,5 @@
 ALTER TABLE `guardian_dev`.`guardian_event` 
-DROP FOREIGN KEY `fk_guardian_event_clients1`
+DROP FOREIGN KEY `fk_guardian_event_clients1`;
 
 ALTER TABLE `guardian_dev`.`guardian_event` 
 CHANGE COLUMN `timestamp` `timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
