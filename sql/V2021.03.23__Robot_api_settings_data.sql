a297fbda581444331a24c88323cc733fd6c24481
Marleen Overkamp <m.overkamp@movements.nl>
2021-03-23 17:51:19 +0100

    Settings data for the robot integration service to call the report request update API.

---
+++ sql/V2021.03.23__Robot_api_settings_data.sql
@@ -0,0 +1,8 @@
+/**
+ * Author:  moverkamp
+ * Created: Mar 23, 2021
+ */
+
+INSERT INTO `settings` (`id`, `version`, `setting_key`, `setting_value`, `keytype`) VALUES
+	('4aa94409-a052-4e7c-8474-1ea604c722a0', 0, 'ROBOTINT_API_HOST', 'https://api.guardian.jef.it', 'STRING'),
+	('0a4d1794-a7d7-4837-9aae-1fc03c426768', 0, 'ROBOTINT_API_URL', '/dev', 'STRING');
