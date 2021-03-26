0efb20e7722e50353f8383dc7233866caba9c06f
Marleen Overkamp <m.overkamp@movements.nl>
2021-03-26 10:43:28 +0100

    New setting for displaying images in emails.

---
+++ sql/V2021.03.25__Email_assets_base_url_setting_data.sql
@@ -0,0 +1,7 @@
+/**
+ * Author:  moverkamp
+ * Created: Mar 25, 2021
+ */
+
+INSERT INTO `settings` (`id`, `version`, `setting_key`, `setting_value`, `keytype`) VALUES
+	('ea65f89d-0341-4789-8695-7f25b2ef371e', 0, 'EMAIL_ASSETS_BASE_URL', 'http://api-guardian.onlyoneif.com/email-assets', 'STRING');
